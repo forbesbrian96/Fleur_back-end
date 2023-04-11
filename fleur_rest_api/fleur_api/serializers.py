e911081195a05bac5f5545e0f33c6ada8b306cbc
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-04-11 16:21:59 -0400

    fixed url problem, migrated

--- fleur_rest_api/fleur_api/serializers.py
+++ fleur_rest_api/fleur_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Plant
 class PlantSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Plant # tell django which model to use
-        fields = ('id', 'name') # tell django which fields to include
+        fields = ('id', 'name', 'image') # tell django which fields to include
