f37bcc71c6c3a7746deb3ea9df94f89d01e11c99
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-04-28 10:54:00 -0400

    altered settings.py

--- fleur_rest_api/settings.py
+++ fleur_rest_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&*h%zfi)&bwp-w%ui3baq$cbq*vptvw=ult#cg1pgnb5dx_&6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'secure-journey-08415.herokuapp.com']
 
 
 # Application definition
@@ -78,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'fleur_rest_api.wsgi.application'
+WSGI_APPLICATION = 'fleur_back-end.wsgi.application'
 
 
 # Database
